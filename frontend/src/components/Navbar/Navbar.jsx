71069d588dc6cfe54fe1cad7dd3023316225a727
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-04-30 19:14:41 -0400

    navbar css completedgit add .

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
-
-
+import {HiOutlineMenuAlt1} from "react-icons/hi";
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -13,20 +12,22 @@ const Navbar = () => {
       <div className='container navbar-content flex'>
         <div className='brand-and-toggler flex flex-sb'>
           <Link to = "/" className='navbar-brand flex'>
-            <span className='text-uppercase fw-7 fs-24 ls-1'>Bookworm</span>
+            <span className='fw-7 fs-24 ls-1'>BOOKWORM</span>
           </Link>
           <button type = "button" className='navbar-toggler-btn' onClick={handleNavbar}>
-            
+            <HiOutlineMenuAlt1 size = {30} style = {{
+              color: `${toggleMenu ? "#fff" : "#010101"}`
+            }} />
           </button>
         </div>
 
         <div className={toggleMenu ? "navbar-collapse show-navbar-collapse" : "navbar-collapse"}>
           <ul className = "navbar-nav">
             <li className='nav-item'>
-              <Link to = "book" >Home</Link>
+              <Link to = "book" className='nav-link text-uppercase text-white fs-22 fw-5 ls-1'>Home</Link>
             </li>
             <li className='nav-item'>
-              <Link to = "about" >About</Link>
+              <Link to = "about" className='nav-link text-uppercase text-white fs-22 fw-2 ls-1'>about</Link>
             </li>
           </ul>
         </div>
