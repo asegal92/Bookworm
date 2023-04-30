4aa8e295a791da4c094ccba9f8e489c0c59624b7
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-04-29 20:55:21 -0400

    Another commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react"
+import React, {useState} from "react";
+import {Link} from 'react-router-dom';
+import "./Navbar.css";
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const handlenavbar = () => setToggleMenu(!toggleMenu);    
     return (
-        <div>Navbar</div>
+       
+                </div>
+            </div>
+        </nav>
     )
 }
 
