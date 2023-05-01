29b70109df038fbc25b02673c8cf837dab344709
anastasiia92 <nastia.dmytryshyna@gmail.com>
2023-04-30 20:52:46 -0400

    search form completed

--- frontend/src/components/SearchForm/SearchForm.jsx
+++ frontend/src/components/SearchForm/SearchForm.jsx
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useRef } from 'react';
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom';
+import "./SearchForm.css";
 
 const SearchForm = () => {
+    const searchText = useRef('');
+    const navigate = useNavigate();
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      navigate(`/search?q=${searchText.current.value}`);
+    };
+
     return (
-        <div>SearchForm</div>
+        <div className='search-form'>
+         <div className='container'>
+          <div className='search-form-content'>
+            <form className='search-form' onSubmit={handleSubmit}>
+              <div className='search-form-elem flex flex-sb bg-white'>
+                <input type = "text" className='form-control' placeholder="I'm looking for ..." ref = {searchText} />
+                <button type = "submit" className='flex flex-c' onClick={handleSubmit}>
+                  <FaSearch className='text-purple' size = {30} />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     )
-}
+  }
+  
 
 export default SearchForm
